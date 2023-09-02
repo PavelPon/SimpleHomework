9a240f4fbfbc137a1e843451c1da79175ab2dfdc
Pavel Ponomarev <weizylol92@gmail.com>
2023-09-02 11:23:57 +0300

    Initial Commit

---
+++ Simple homework/Simple_homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_homeworkApp.swift
+//  Simple homework
+//
+//  Created by Павел Пономарёв on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Simple_homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
